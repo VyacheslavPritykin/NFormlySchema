462f54e955d15f2495f058cdf95a5ca56a8f677b
Vyacheslav Pritykin <vyacheslavp@leadsmarket.com>
2020-10-15 00:24:18 +0300

    Remove old code

--- FormlySchema.Generation.CSharp/Extensions/TypeExtensions.cs
+++ FormlySchema.Generation.CSharp/Extensions/TypeExtensions.cs
@@ -1,22 +1,11 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 
 namespace FormlySchema.Generation.CSharp
 {
     internal static class TypeExtensions
     {
-        public static bool IsCollection(this Type type)
-        {
-            return type.GetInterface(nameof(ICollection)) != null;
-
-            var elementType = type.GetElementType();
-            if (elementType == null)
-                return false;
-
-            return typeof(ICollection<>)
-                .MakeGenericType(elementType)
-                .IsAssignableFrom(type);
-        }
+        public static bool IsCollection(this Type type) =>
+            type.GetInterface(nameof(ICollection)) != null;
     }
 }
